492cf136c8ac7fc9f5dbc79719842d8bf7d4f5b6
radiium <remip.dev@gmail.com>
2019-02-21 23:09:09 +0100

    Fix utils dependency

--- utils/pass.js
+++ utils/pass.js
@@ -1,4 +1,4 @@
 #!/usr/bin/env node
-var bcrypt = require('./bCrypt');
+var bcrypt = require('bcrypt-nodejs');
 var hash = bcrypt.hashSync(process.argv[2], bcrypt.genSaltSync(8), null);
 console.log(hash)
